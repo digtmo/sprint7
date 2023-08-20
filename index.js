9a0a06e1a1325f13c621be5ed589fb90835b9c93
digtmo <davidtorresim@gmail.com>
2023-08-19 22:33:37 -0400

    first

---
+++ index.js
@@ -0,0 +1,65 @@
+import express, { urlencoded } from 'express';
+import { crearTransferencia, crearUsuario, mostrarUsuarios, editarUsuario, eliminarUsuario, mostrarTransferencias } from './models/bancosolar.js';
+const app = express();
+const port = 3000;
+app.use(express.static("public"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+ 
+
+app.get("/", (req,res)=>{
+    res.render("index")
+});
+
+
+app.post("/usuario", async (req,res)=>{
+    let nombre = req.body.nombre
+    let balance = req.body.balance
+    crearUsuario(nombre,balance)
+    res.send(await mostrarUsuarios())
+});
+
+
+app.get("/usuarios", async (req,res)=>{
+    res.send(await mostrarUsuarios())
+});
+
+app.put("/usuario", async (req,res)=>{ 
+    let id = req.query.id
+    let nombre = req.body.nombre
+    let balance = req.body.balance 
+    editarUsuario(id,nombre,balance)
+    res.send(await mostrarUsuarios())
+});
+
+app.delete("/usuario", async (req,res)=>{ 
+    let id = req.query.id
+    eliminarUsuario(id)
+    res.send(await mostrarUsuarios())
+});
+
+app.post("/transferencia", async (req,res)=>{
+    console.log("BODY :",req.body)
+    let emisor = parseInt(req.body.emisor)
+    let receptor = parseInt(req.body.receptor)
+    let monto = parseInt(req.body.monto)
+    crearTransferencia(emisor,receptor,monto) 
+    res.send(await mostrarTransferencias())
+});
+
+app.get("/transferencias", async (req,res)=>{
+    res.send(await mostrarTransferencias())
+});
+
+
+
+
+
+
+app.listen(port,() => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+  });
+
+
+
---
+++ models/bancosolar.js
@@ -0,0 +1,89 @@
+import { Sequelize, DataTypes } from "sequelize";
+const sequelize = new Sequelize('bancosolar', 'postgres','',{host:'localhost', password:'123momiaes', dialect:'postgres'});
+
+export const transferencia = sequelize.define('transferencias',{
+    emisor:{type:DataTypes.INTEGER},
+    receptor:{type:DataTypes.INTEGER},
+    monto:{type:DataTypes.FLOAT},
+});
+
+
+export const usuario = sequelize.define('usuarios',{
+    nombre:{type:DataTypes.STRING},
+    balance:{type:DataTypes.FLOAT},
+});
+
+
+transferencia.belongsTo(usuario, { foreignKey: 'emisor', as: 'EmisorUsuario' });
+transferencia.belongsTo(usuario, { foreignKey: 'receptor', as: 'ReceptorUsuario' });
+
+ async function crearUsuario(nombre,balance){
+    await usuario.create({nombre,balance})
+}
+
+ async function crearTransferencia(emisor,receptor,monto){
+    // verificar que el emisor tenga balance > monto
+    const auxEmisor = await usuario.findByPk(emisor)
+    let balanceEmisor = auxEmisor.balance 
+    if (balanceEmisor >= monto){
+       
+        // restar monto a balance del emisor 
+        let balanceEmisorActual = balanceEmisor - monto
+        editarBalanceUsuario(emisor,balanceEmisorActual)
+        
+        // sumar monto al receptor
+        const auxReceptor = await usuario.findByPk(receptor)
+        let balanceReceptor = auxReceptor.balance
+        let balanceReceptorActual = balanceReceptor + monto
+        editarBalanceUsuario(receptor,balanceReceptorActual)
+        await transferencia.create({emisor,receptor,monto}) 
+    } else {
+       console.log("Saldo Insuficiente")
+    }
+
+}
+
+async function editarBalanceUsuario(id,balance){
+    const aux = await usuario.findByPk(id)
+    aux.balance = balance
+    return aux.save()
+}
+
+async function mostrarUsuarios(){
+    return await usuario.findAll()
+}
+
+async function editarUsuario(id,nombre,balance){
+    const aux = await usuario.findByPk(id)
+    aux.nombre = nombre
+    aux.balance = balance
+    return aux.save()
+}
+
+async function eliminarUsuario(id){
+    const aux = await usuario.findByPk(id)
+    return await aux.destroy(id)
+}
+
+async function mostrarTransferencias() {
+    try {
+      const transferenciasConUsuarios = await transferencia.findAll({
+        include: [
+          { model: usuario, as: 'EmisorUsuario', attributes: ['nombre'] },
+          { model: usuario, as: 'ReceptorUsuario', attributes: ['nombre'] }
+        ]
+      });
+  
+      return transferenciasConUsuarios;
+    } catch (error) {
+      console.error('Error al obtener transferencias:', error);
+    }
+  }
+  
+
+
+
+
+export {crearUsuario,crearTransferencia, mostrarUsuarios, editarUsuario, eliminarUsuario, mostrarTransferencias}
+
+sequelize.sync()
